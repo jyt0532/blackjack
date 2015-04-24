6f7d14aafe3cc052aa22e6a956644d1e98717076
Bo-Yu, Chiang <jyt0532@gmail.com>
2015-04-23 23:02:43 -0500

    delete all the debug messages

--- simulate.cpp
+++ simulate.cpp
@@ -1,38 +1,39 @@
 #include<iostream>
 #include <unistd.h>
 #include "BlackjackGame.cpp"
+#define NUM_OF_ROUNDS 1000000
 using namespace std;
-unsigned int microseconds;
 int main(){
-    BlackjackGame game(8, 0.25);
-    Player p1(10);
+    BlackjackGame game(8, 0.25, 1);
+    Player p1(10, 1);
     p1.set_strategy(hard, soft, split);
-    Player p2(10);
-    soft[13][3] = D_H;
-    soft[13][4] = D_H;
+    Player p2(10, 5);
     p2.set_strategy(hard, soft, split);
-    Player p3(10);
-    hard[12][3] = STAND;
-    hard[12][3] = STAND;
+/*
+ *Modify any stragety as you want to see the expected return
+ * 
+ *   hard[12][3] = STAND;
+ *   soft[13][4] = D_H;
+ *   split[8][10] = HIT;
+ *   split[8][1] = HIT;
+*/
+    Player p3(10, 0);
     p3.set_strategy(hard, soft, split);
 
-    //cout << p1.get_wager() << endl;
     game.add_player(p1);
     game.add_player(p2);
     game.add_player(p3);
-//    game.addPlayer(p2);
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < NUM_OF_ROUNDS; i++){
         game.play_round();
-        //cout << game.players[0].get_money() << endl;
-        //usleep(10000);
-//        cout << "1:" << game.players[0].get_money() << endl;
-//        cout << "2:" << game.players[1].get_money() << endl;
-//        cout << "3:" << game.players[2].get_money() << endl;
-        cout << i << endl;
+        game.players[0].modify_bet_by_count(game.doc.get_running_count());
     }
+
     cout << "1:" << game.players[0].get_money() << endl;
     cout << "2:" << game.players[1].get_money() << endl;
     cout << "3:" << game.players[2].get_money() << endl;
+    cout << "1:" << game.players[0].get_sidebet_money() << endl;
+    cout << "2:" << game.players[1].get_sidebet_money() << endl;
+    cout << "3:" << game.players[2].get_sidebet_money() << endl;
 
     return 0;
 }
